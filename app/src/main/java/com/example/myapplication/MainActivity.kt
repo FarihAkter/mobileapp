a8cbb0f9f20003bcfaf57e6960285ffc3241b06a
Fariha Akter <farihakter098@gmail.com>
2025-08-09 05:47:26 +0600

    Initial commit - MyApplication

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,15 +6,31 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var clickCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // links to activity_main.xml
+        setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
 
+        val messages = listOf(
+            "Keep going!",
+            "You're doing great!",
+            "Awesome clicks!",
+            "Click master!",
+            "You rock!"
+        )
+
         button.setOnClickListener {
-            textView.text = "You clicked the button!"
+            clickCount++
+            button.text = "Clicked $clickCount times"
+
+            // Change message every 3 clicks
+            val msgIndex = (clickCount / 3) % messages.size
+            textView.text = messages[msgIndex]
         }
     }
 }
